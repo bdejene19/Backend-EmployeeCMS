62d690e9914c98c01198a1994620c082a090c85b
Bemnet Dejene <bdejene19@gmail.com>
2022-02-20 17:12:42 -0500

    fixed up foreign key relationships in sql tables. Need to still fix self referencing key seeds.

--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -1,22 +1,24 @@
 INSERT INTO department(dept_name)
 VALUES  ('Engineer'),
+        ('Lead Software Engineer'),
         ("Administration"),
-        ("Delivery"),
+        ("Receptionist"),
         ("Communications"),
-        ('Sales');
+        ('Sales Associate'),
+        ('Sales Manager');
         
 INSERT INTO employee_role(title, salary, dept_id)
 VALUES  ('Engineer', 80000.0, 1),
-        ("Receptionist", 60000.0, 2),
-        ("Sales Manager", 90000, 3),
-        ("Lead Software Engineer", 120000.0, 1),
-        ('Sales Associate', 70000.0, 3);
+        ("Receptionist", 60000.0, 4),
+        ("Sales Manager", 90000.0, 7),
+        ("Lead Software Engineer", 120000.0, 2),
+        ('Sales Associate', 70000.0, 6);
 
-INSERT INTO employee(first_name, last_name, manager_id)
-VALUES ("Bement", "Dejene", 3232    );
---         ("Bruk", "Dejene", 102, 200),
---         ("Teddy", "Dejene", 103, 300),
---         ("Dejene", "Sahle", 101, 100),
---         ("Justin", "Kim", 101, 300);
+INSERT INTO employee(first_name, last_name, role_id)
+VALUES ("Bement", "Dejene", 1, 2),
+        ("Bruk", "Dejene", 4),
+        ("Teddy", "Dejene", 7),
+        ("Dejene", "Sahle", 2),
+        ("Justin", "Kim", 6);
 
         
