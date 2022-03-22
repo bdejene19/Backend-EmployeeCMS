acd0275dabcdcec9f21eec83a30ab2900ff49142
Bemnet Dejene <bdejene19@gmail.com>
2022-03-22 19:38:51 -0400

    refactored prompts due to sloppy logic. Tried to focus functions and break down process into more readable fashion

--- Develop/prompts/add.js
+++ Develop/prompts/add.js
@@ -1,114 +1,125 @@
 const mysql = require("mysql2");
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const util = require("util");
 const db = mysql.createConnection(
-    {
+  {
     host: "localhost",
     user: "root",
-    password: '1234',
-    database: 'businessoverlook_db',
-    },
-    console.log('connected to businessoverlook_db in mysql')
-)
+    password: "1234",
+    database: "businessoverlook_db",
+  },
+  console.log("connected to businessoverlook_db in mysql")
+);
+db.connect();
 
 /**
  * Maps array and returns new array of prompt objects
  * @param {[]} questionsArr Array of questions to be asked in prompt
  * @returns new array of prompt objects (properties: type, message, name);
  */
- const createPromptObjectsArray = (questionsArr) => {
-    const promptArr = questionsArr.map((question, index) =>  {
-        if (index === questionsArr.length - 1 && questionsArr.length > 1) {
-            return {
-                type: 'list',
-                message: 'Select a Role to add to:',
-                choices: question,
-                name: 'roleSelect',
-            }
-        } else {
-            let nameVal = '';
-            if (questionsArr.length > 1) {
-                nameVal = question.split(" ")[3];
-            } else {
-                nameVal = 'deptName';
-            }
-            return {
-                type: 'input',
-                message: `${question}`,
-                name: `${nameVal}`,
-            }
+//  const allRoles = ["Engineer", "Lead Software Engineer", "Administration", "Receptionist", "Communications", "Sales Associate", "Sales  Manager"];
 
-        }
-       
-    })
-    return promptArr;
-}
-let promptQs = [];
-const deptQs = ["What is the name of your department?"];
-const roleQs = ['What is the name of the role?', 'What is the salary of the role?', ['Engineer', "Sales", "Customer Services", "Administration"]]
-const addRolePrompt = (role) => {
-    // raw list questions 'Which department does the role belong to?'
-    if (role === 'Role') {
-        promptQs = roleQs
-    } else if (role === "Department") {
-        promptQs = deptQs
+const createPromptObjectsArray = (questionsArr) => {
+  const promptArr = questionsArr.map((question, index) => {
+    if (index === questionsArr.length - 1 && questionsArr.length > 1) {
+      let listChoices = questionsArr[questionsArr.length - 1].map((item) => ({
+        name: item.dept_name,
+        value: item.id,
+      }));
+      return {
+        type: "list",
+        message: "Select a Role to add to:",
+        choices: listChoices,
+        name: "roleSelect",
+      };
     } else {
-
+      let nameVal = "";
+      if (questionsArr.length > 1) {
+        nameVal = question.split(" ")[3];
+      } else {
+        nameVal = "deptName";
+      }
+      return {
+        type: "input",
+        message: `${question}`,
+        name: `${nameVal}`,
+      };
     }
+  });
+  return promptArr;
+};
 
-    const promptObjs = createPromptObjectsArray(promptQs);
-    return inquirer.prompt(promptObjs)
-}
-const allRoles = ["Engineer", "Lead Software Engineer", "Administration", "Receptionist", "Communications", "Sales Associate", "Sales  Manager"]
-const addToTable = (name) => {
-    let nameArr = name.split(" ");
-    let userQuery = nameArr[nameArr.length - 1];
-    let dbTableName = '';
-    let dept_id = 0;
-    let dbColNames = '';
-    let insertVals = '';
-    let sqlStr = ``;
-    console.log(`\nAdd a ${userQuery}\n`);
-    if (userQuery === 'Department') {
-        dbTableName = 'department';
-        dbColNames = 'dept_name';
-    } 
-    else if (userQuery === 'Role') {
-        dbTableName = 'employee_role';
-        dbColNames = 'title, salary, dept_id';
-    } 
-    else {
-        dbTableName = 'employee';
-    }
+const addRolePrompt = async (deptArr) => {
+  let promptQs = [];
 
-    // prompts for new employee
-    addRolePrompt(userQuery).then(roleData => {
-    // sql insertion after prompts
-        if (userQuery === 'Department') {
-            const { deptName } = roleData;
-            insertVals = `"${deptName}"`;
-            // sqlStr = `INSERT INTO ${dbTableName}(${dbColNames}) VALUES (${insertVals});`;
+  // raw list questions 'Which department does the role belong to?'
+  const roleQs = [
+    "What is the name of the role?",
+    "What is the salary of the role?",
+    await deptArr,
+  ];
+  promptQs = roleQs; //
 
-        }
-        else if (userQuery === 'Role') {
-            const { name, salary, roleSelect } = roleData;
-            allRoles.map((role, index) => {
-                if (role === roleSelect) {
-                    dept_id = index + 1;
-                }
-            })
-            insertVals = `"${name}", ${parseInt(salary)}, ${parseInt(dept_id)}`;            
-        }
-        sqlStr = `INSERT INTO ${dbTableName}(${dbColNames}) VALUES (${insertVals});`;
+  const promptObjs = createPromptObjectsArray(promptQs);
+  return inquirer.prompt(promptObjs);
+};
 
-        console.log('my sql str: ', sqlStr);
-        db.query(`${sqlStr}`, (err, data) => {
-            err ? console.log(err) : console.log(`success adding new ${name} role to database${dbTableName}.`);
-            console.log(data);
-        })
+const printText = (textContent) => {
+  let text = `____________\n${textContent}\n____________`;
+  return text;
+};
+
+const addDept = (deptName) => {
+  let sql_logic = `INSERT INTO department(dept_name) VALUES ("${deptName}")`;
+  db.query(`${sql_logic}`, (err, data) => {
+    console.log(
+      printText(`Successfully added ${deptName} to department database`)
+    );
+    return db.query(`SELECT * FROM department`, (err, data) => {
+      console.table(data);
     });
-    
-}
+  });
+};
+
+const addToTable = (name) => {
+  let nameArr = name.split(" ");
+  let userQuery = nameArr[nameArr.length - 1];
+  let dbTableName = "";
+  if (userQuery === "Department") {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the name of the department you want to add?",
+          name: "newDept",
+        },
+      ])
+      .then((deptName) => {
+        return addDept(deptName.newDept);
+      });
+  } else if (userQuery === "Role") {
+    db.query(`SELECT * FROM department`, (err, data) => {
+      addRolePrompt(data).then((newRole) => {
+        console.log(newRole);
+        const { name, salary, roleSelect } = newRole;
+        let sql_logic = `INSERT INTO employee_role(title, salary, dept_id) VALUES("${name}", ${salary}, ${roleSelect} ) `;
+        return db.query(sql_logic, (err, data) => {
+          console.log(
+            printText(
+              `Successfully added ${name} to employee_role table in database`
+            )
+          );
+        });
+      });
+    });
+  } else {
+    dbTableName = "employee";
+  }
+
+  // // prompts for new employee
+};
 
 module.exports = {
-    addToTable,
-}
+  addToTable,
+  db,
+};
