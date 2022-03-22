7415e899192533016d4da91d41d50abc7d1c15b7
Bemnet Dejene <bdejene19@gmail.com>
2022-03-22 19:36:47 -0400

    finished seeding database with foreign keys and self pointing foreign keys

--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -1,24 +1,23 @@
 INSERT INTO department(dept_name)
 VALUES  ('Engineer'),
-        ('Lead Software Engineer'),
-        ("Administration"),
-        ("Receptionist"),
+        ('Administration'),
         ("Communications"),
-        ('Sales Associate'),
-        ('Sales Manager');
-        
-INSERT INTO employee_role(title, salary, dept_id)
-VALUES  ('Engineer', 80000.0, 1),
-        ("Receptionist", 60000.0, 4),
-        ("Sales Manager", 90000.0, 7),
-        ("Lead Software Engineer", 120000.0, 2),
-        ('Sales Associate', 70000.0, 6);
+        ("Sales"),
+        ("Finance"),
+        ('Executive');
 
-INSERT INTO employee(first_name, last_name, role_id)
-VALUES  ("Bement", "Dejene", 1,),
-        ("Bruk", "Dejene", 4),
-        ("Teddy", "Dejene", 3),
-        ("Dejene", "Sahle", 2),
-        ("Justin", "Kim", 5);
+INSERT INTO employee_role(title, salary, dept_id)
+VALUES  ('Software Developer', 80000.0, 1),
+        ("Receptionist", 60000.0, 3),
+        ("Sales Manager", 90000.0, 4),
+        ("Lead Software Engineer", 120000.0, 1),
+        ('Sales Associate', 70000.0, 4);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES  ("Bement", "Dejene", 1, 1),
+        ("Bruk", "Dejene", 1, 2),
+        ("Teddy", "Dejene", 3, 2),
+        ("Dejene", "Sahle", 2, 2),
+        ("Justin", "Kim", 5, 3);
 
         
