03bcf16fed60caf0a3dca02a53e5901a2ab50fa0
Bemnet Dejene <bdejene19@gmail.com>
2022-03-22 19:37:13 -0400

    reformatted index.js to handle more logic outside of main file

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,31 @@
-const { addToTable } = require('./prompts/add');
-const { addViewDecision } = require('./prompts/addOrView');
+const { addToTable, db, addDept } = require("./prompts/add");
+const { addViewDecision } = require("./prompts/addOrView");
 const { viewTableFromDb } = require("./prompts/view");
 
-addViewDecision().then(decision => {
-    const decisionArr = decision.option.split(" ");
-    const addOrView = decisionArr[0];
-    if (addOrView === 'View') {
+const inquirer = require("inquirer");
+
+const introPrompt = () => {
+  return inquirer.prompt([
+    {
+      type: "confirm",
+      message: "View Database?",
+      name: "isViewed",
+    },
+  ]);
+};
+
+introPrompt().then((view) => {
+  if (view) {
+    addViewDecision().then((decision) => {
+      const decisionArr = decision.option.split(" ");
+      const addOrView = decisionArr[0];
+      if (addOrView === "View") {
         viewTableFromDb(decision.option);
-    } else if (addOrView === 'Add') {
+      } else if (addOrView === "Add") {
         addToTable(decision.option);
-    }
+      }
+    });
+  }
 });
 
-module.exports = {
-}
+module.exports = {};
